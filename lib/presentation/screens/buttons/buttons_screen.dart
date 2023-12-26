690645efbe93c32437a8084610b699a297618af1
Jhonatan Lopez <jlopez@develsystems.com>
2023-12-25 18:10:55 -0600

    feat: Configuracion de botones

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -1,28 +1,103 @@
-
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class ButtonScreen extends StatelessWidget {
-
   static const String name = "button_screen";
-  
+
   const ButtonScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Buttons Screen'),
-      ),
-      body: _ButtonsView(),
-    );
+        appBar: AppBar(
+          title: const Text('Buttons Screen'),
+        ),
+        body: _ButtonsView(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            context.pop();
+          },
+          child: const Icon(Icons.arrow_back_ios_new_rounded),
+        ));
   }
 }
 
 class _ButtonsView extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+
+    final colors = Theme.of(context).colorScheme;
+
+    return SizedBox(
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+        child: Wrap(
+          spacing: 10,
+          runSpacing: 10,
+          alignment: WrapAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text("Elevated Button"),
+            ),
+            const ElevatedButton(
+              onPressed: null,
+              child: Text("Elevated Button Disabled"),
+            ),
+            ElevatedButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.access_alarm_rounded),
+              label: const Text("Elevated Button Icon"),
+            ),
+
+            FilledButton(
+              onPressed: () {},
+              child: const Text("Filled Button"),
+            ),
+            FilledButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.accessibility_new_rounded),
+              label: const Text("Filled Button Icon"),
+            ),
+
+            OutlinedButton(
+              onPressed: () {},
+              child: const Text("Outlined Button"),
+            ),
+            OutlinedButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.terminal_rounded),
+              label: const Text("Outlined Button Icon"),
+            ),
+
+            TextButton(
+              onPressed: () {},
+              child: const Text("Text Button"),
+            ),
+            TextButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.text_snippet_rounded),
+              label: const Text("Text Button Icon"),
+            ),
+
+            // TODO: Custom button
+
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.add),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.app_registration_rounded),
+              style: ButtonStyle(
+                backgroundColor: MaterialStatePropertyAll(colors.primary),
+                iconColor: const MaterialStatePropertyAll(Colors.white),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
-  
 }
